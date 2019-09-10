c72c19f9379aa8ba332a17f1471af895e9430345
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 18:52:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
 
-    def change 
+    def change
         create_table :haunted_houses do |t|
             t.string :name
             t.string :location
