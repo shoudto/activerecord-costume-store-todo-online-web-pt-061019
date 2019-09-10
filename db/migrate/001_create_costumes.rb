149097ca5e0b6b09d9b20a314a624225d15fdfef
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 18:26:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,5 +3,5 @@
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
