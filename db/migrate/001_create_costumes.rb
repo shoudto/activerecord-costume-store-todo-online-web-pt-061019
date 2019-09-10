1128ae63cf42a5e613d93e1660107cfb488653e3
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 18:31:11 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,7 +10,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.string :imageurl
+      t.string :image_url
       t.integer :size
     end
   end
